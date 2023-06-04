4e5889e66e9d235e1a645e6347f99de7f3963920
Nika Buzynska <nikabuzynska@gmail.com>
2023-06-04 16:05:16 +0100

    Translate to english

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,13 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
+let orderButton = document.querySelector('.product-demonstration__button');
+let modalWindow = document.querySelector('.modal-container');
+let modal = modalWindow.querySelector('.modal');
 
 navMain.classList.remove('main-nav--nojs');
 navMain.classList.add('main-nav--closed');
 
-navToggle.addEventListener('click', function () {
+function toggleNav() {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
@@ -12,4 +15,20 @@ navToggle.addEventListener('click', function () {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
   }
-});
+};
+
+function showModalWindow(evt) {
+  evt.stopPropagation();
+  modalWindow.classList.remove('modal-container--close');
+  document.addEventListener('click', closeOnOutsideClick);
+};
+
+function closeOnOutsideClick(evt) {
+  if (!modal.contains(evt.target)) {
+    modalWindow.classList.add('modal-container--close');
+    document.removeEventListener('click', closeOnOutsideClick);
+  }
+};
+
+navToggle.addEventListener('click', toggleNav);
+orderButton.addEventListener('click', showModalWindow);
